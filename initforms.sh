6f41fc9df1493e804eec948ca936ef1e7489fe8a
olevole <olevole@olevole.ru>
2018-11-30 20:30:10 +0300

    wip

---
+++ initforms.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+MYDIR="$( /usr/bin/dirname $0 )"
+MYPATH="$( /bin/realpath ${MYDIR} )"
+HELPER="memcached"
+
+. /etc/rc.conf
+
+workdir="${cbsd_workdir}"
+
+set -e
+. ${workdir}/cbsd.conf
+. ${subr}
+set +e
+
+MYPATH="${workdir}/formfile"
+
+[ ! -d "${MYPATH}" ] && err 1 "No such ${MYPATH}"
+[ -f "${MYPATH}/${HELPER}.sqlite" ] && /bin/rm -f "${MYPATH}/${HELPER}.sqlite"
+
+/usr/local/bin/cbsd ${miscdir}/updatesql ${MYPATH}/${HELPER}.sqlite /usr/local/cbsd/share/forms.schema forms
+
+/usr/local/bin/sqlite3 ${MYPATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,1,"tcp_port","tcp_port: default is 11211",'11211','','',1, "maxlen=60", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,2,"udp_port","udp_port: default is 11211",'11211','','',1, "maxlen=60", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,3,"max_connections","max_connections: default is 1024",'1024','','',1, "maxlen=60", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,4,"unix_socket","unix_socket path (/tmp/memcached.sock) or 'undef' to disable unix socket",'undef','','',1, "maxlen=60", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,5,"processorcount","processorcount: number of worker. 'undef' for sets to number of core",'undef','','',1, "maxlen=60", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"use_sasl","Use SASL. Default is no SASL",'2','2','',1, "maxlen=60", "radio", "use_sasl_yesno", "" );
+COMMIT;
+EOF
+
+# Put version
+/usr/local/bin/cbsd ${miscdir}/updatesql ${MYPATH}/${HELPER}.sqlite /usr/local/cbsd/share/forms_system.schema system
+
+# yesno
+/usr/local/bin/cbsd ${miscdir}/updatesql ${MYPATH}/${HELPER}.sqlite /usr/local/cbsd/share/forms_yesno.schema use_sasl_yesno
+
+# Put boolean for use_sasl_yesno
+/usr/local/bin/sqlite3 ${MYPATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+INSERT INTO use_sasl_yesno ( text, order_id ) VALUES ( "yes", 1 );
+INSERT INTO use_sasl_yesno ( text, order_id ) VALUES ( "no", 0 );
+COMMIT;
+EOF
+
+/usr/local/bin/sqlite3 ${MYPATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+INSERT INTO system ( helpername, version, packages, have_restart ) VALUES ( "memcached", "201607", "databases/memcached", "service memcached restart" );
+COMMIT;
+EOF
+
+# long description
+/usr/local/bin/sqlite3 ${MYPATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+UPDATE system SET longdesc='\
+memcached is a high-performance, distributed memory object caching \
+system, generic in nature, but intended for use in speeding up dynamic \
+web applications by alleviating database load. \
+';
+COMMIT;
+EOF
